3adac13c196ec94aadd8a1276d5bddf17e169f0f
Wil-Rs <43270145+Wil-Rs@users.noreply.github.com>
2018-10-05 16:34:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
+import os, time
 _author_ = 'WilRs'
 
-#Tem algumas coisas para corrigir -> Tem operaçoes que tao dando errp
+#Tem algumas coisas para corrigir -> Tem operações que tão dando erro.
 
 def lista_pastas():
     #Imprime os arquivos contidos no pasta na tela.
@@ -33,6 +33,7 @@ while True:
 [ C ] para criar um diretorio.
 [ Q ] para renomear um arquivo/diretorio.
 [ R ] para remover um diretorio.
+[ Y ] para renomear todos os arquivos.
 [ D ] para digitar um endereço.''') 
 
     print()    
@@ -85,7 +86,7 @@ while True:
                 print()
                 
         except OSError:
-           print('-------------------\nDEU ERRADO\n-------------------')
+           print('Por enquanto voce so pode remover pastas vazias.')
 
     elif opcao in 'Qq':
         #Opçao para renomear um diretorio/arquivo.
@@ -105,4 +106,50 @@ while True:
         print('EX: c:/MinhaPasta')
         endereco = input('Digite o endereço: ')
         os.chdir(endereco)
-        
+
+    elif opcao in 'Yy':
+        #Opçao para renomear todos diretorios e arquivos.
+        print()
+        print('''
+Os arquivos e pasta serao renomeados assim:
+
+EX:
+----------- pasta -----------
+nome_que_você_decidiu - 1
+nome_que_você_decidiu - 2
+nome_que_você_decidiu - 3
+nome_que_você_decidiu - 4
+
+--------- arquivos ----------
+nome_que_você_decidiu - 5.txt
+nome_que_você_decidiu - 6.txt
+nome_que_você_decidiu - 7.txt
+nome_que_você_decidiu - 8.txt
+
+-----------------------------
+OBS: OS NÚMEROS PODEM FICAR
+DIFERENTES --> NAO SEQUENCIAL
+
+                ''')
+        print()
+        nomes = input('Digite o nome para renomer todos os arquivos: ')
+        try:
+            kkk = 0
+            for teste in ( os.listdir() ):
+                
+                if os.path.isfile(teste):
+                    
+                    #PARA PEGAR O PONTO DE TRAZ PRA FRENTE
+                    oito = teste[::-1].index('.')
+                    oito *= -1
+                    oito -= 1
+                    
+                    os.rename(teste, '{} - {}{} '.format(nomes, kkk, teste[oito:]))
+                    
+                else:
+                    os.rename(teste, '{} - {} '.format(nomes, kkk))
+
+                kkk += 1
+
+        except FileExistsError:
+            pass
